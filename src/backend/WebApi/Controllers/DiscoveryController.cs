4ea15f8f5a90729d4fa18fd9e33b5555a5fde65e
shreya sinha <sinhashreya707@gmail.com>
2026-09-03 19:19:17 +0530

    changes2

--- src/backend/WebApi/Controllers/DiscoveryController.cs
+++ src/backend/WebApi/Controllers/DiscoveryController.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Application.Features.Discovery.Queries.GetSemanticSearch;
+using MediatR;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
-/// <summary>
-/// Summary description for Order
-/// </summary>
-public class Order
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class DiscoveryController : ControllerBase
 {
-    public Order()
+    private readonly IMediator _mediator;
+
+    public DiscoveryController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost("search")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> ExecuteSemanticSearch(
+        [FromBody] GetSemanticSearchQuery query,
+        CancellationToken cancellationToken)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        var response = await _mediator.Send(query, cancellationToken);
+        return Ok(response);
     }
 }
