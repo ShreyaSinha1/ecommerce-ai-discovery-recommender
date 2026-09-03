8d11673930aaaa05746f4497532063afa745f17a
shreya sinha <sinhashreya707@gmail.com>
2026-09-03 18:26:03 +0530

    changes

---
+++ src/backend/Core/Domain/Entities/ProductEmbedding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ProductEmbedding
+/// </summary>
+public class ProductEmbedding
+{
+    public ProductEmbedding()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
